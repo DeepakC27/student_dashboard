108c6176ca871dfc9155ebc9870ba67a48a2fe18
deepakc <deepak.c@servify.in>
2020-10-22 19:50:58 +0530

    common css defined with variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './Components/Header'
+import Profile from './Components/Profile'
+import PerformanceComp from './Components/PerformanceComp'
 
-function App() {
+const App: React.FC = () => {
+  const [studentDetails] = useState({
+    Name: 'Deepak',
+    EmailId: 'chaudharideepak10@hotmail.com',
+    MobileNo: 9028325881,
+    BatchNo: 'B'
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <main>
+        <section>
+          <Profile {...studentDetails} />
+        </section>
+        <section>
+          <PerformanceComp />
+        </section>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
