462dd740a27b7d3aa5ccb46d2371ea85d84d9265
deepakc <deepak.c@servify.in>
2020-11-22 20:43:16 +0530

    User Profile comp added

---
+++ src/Components/Profile/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Card from '../../Common/Card'
+import './index.scss'
+
+interface Props {
+  Name: string,
+  EmailId: string,
+  MobileNo: number,
+  BatchNo: string  
+}
+
+const Profile: React.FC<Props> = ({ Name, EmailId, MobileNo, BatchNo}) => {
+  return (
+    <Card className='student-profile-container'>
+      <div>
+        <div className='field-label'>Name</div>
+        <div>{Name}</div>
+      </div>
+      <div>
+        <div className='field-label'>EmailId</div>
+        <div>{EmailId}</div>
+      </div>
+      <div>
+        <div className='field-label'>MobileNo</div>
+        <div>{MobileNo}</div>
+      </div>
+      <div>
+        <div className='field-label'>BatchNo</div>
+        <div>{BatchNo}</div>
+      </div>
+    </Card>
+  )
+}
+
+export default Profile
