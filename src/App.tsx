b07830c0ca2fab63b17c4ad0967e95c6661da6ad
deepakc <deepak.c@servify.in>
2020-11-22 22:29:29 +0530

    styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Components/Header'
 import Profile from './Components/Profile'
 import PerformanceComp from './Components/PerformanceComp'
+import { postRequestAsync } from './Api'
 
 const App: React.FC = () => {
   const [studentDetails] = useState({
@@ -10,6 +11,19 @@ const App: React.FC = () => {
     MobileNo: 9028325881,
     BatchNo: 'B'
   })
+
+  useEffect(() => {
+    const getStudentList = () => {
+      postRequestAsync('/getSubjects', {})
+      .then(response => {
+        console.log('resp: ', response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    getStudentList()
+  }, [studentDetails])
   
   return (
     <div className='App'>
